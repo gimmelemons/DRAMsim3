38ee553cf0387bf16ac52db6e6b1dc85df53aab0
gimmelemons <65401076+gimmelemons@users.noreply.github.com>
2023-02-08 16:11:54 +0100

    add build

---
+++ build/CMakeFiles/dramsim3main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hari/HDD/ARCO/powersim/forked-dramsim/src/cpu.cc" "CMakeFiles/dramsim3main.dir/src/cpu.cc.o" "gcc" "CMakeFiles/dramsim3main.dir/src/cpu.cc.o.d"
+  "/home/hari/HDD/ARCO/powersim/forked-dramsim/src/main.cc" "CMakeFiles/dramsim3main.dir/src/main.cc.o" "gcc" "CMakeFiles/dramsim3main.dir/src/main.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hari/HDD/ARCO/powersim/forked-dramsim/build/CMakeFiles/dramsim3.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/dramsim3test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hari/HDD/ARCO/powersim/forked-dramsim/tests/test_config.cc" "CMakeFiles/dramsim3test.dir/tests/test_config.cc.o" "gcc" "CMakeFiles/dramsim3test.dir/tests/test_config.cc.o.d"
+  "/home/hari/HDD/ARCO/powersim/forked-dramsim/tests/test_dramsys.cc" "CMakeFiles/dramsim3test.dir/tests/test_dramsys.cc.o" "gcc" "CMakeFiles/dramsim3test.dir/tests/test_dramsys.cc.o.d"
+  "/home/hari/HDD/ARCO/powersim/forked-dramsim/tests/test_hmcsys.cc" "CMakeFiles/dramsim3test.dir/tests/test_hmcsys.cc.o" "gcc" "CMakeFiles/dramsim3test.dir/tests/test_hmcsys.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hari/HDD/ARCO/powersim/forked-dramsim/build/CMakeFiles/dramsim3.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
